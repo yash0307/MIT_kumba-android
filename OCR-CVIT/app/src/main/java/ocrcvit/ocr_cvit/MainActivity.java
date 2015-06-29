186b9251ac60ad09a4dd05461149e12139e54c57
yashvardhanpatel07 <yash.patel@students.iiit.ac.in>
2015-06-29 14:56:20 +0530

    changes

--- OCR-CVIT/app/src/main/java/ocrcvit/ocr_cvit/MainActivity.java
+++ OCR-CVIT/app/src/main/java/ocrcvit/ocr_cvit/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.app.AlertDialog;
 import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -13,6 +14,7 @@ import android.graphics.Canvas;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
@@ -23,6 +25,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
@@ -67,24 +70,243 @@ public class MainActivity extends Activity {
 
     private String globResponseString;
 
+    final Context context = this;
 
+    public static
+    String unescape_perl_string(String oldstr) {
 
+        StringBuffer newstr = new StringBuffer(oldstr.length());
+
+        boolean saw_backslash = false;
+
+        for (int i = 0; i < oldstr.length(); i++) {
+            int cp = oldstr.codePointAt(i);
+            if (oldstr.codePointAt(i) > Character.MAX_VALUE) {
+                i++;
+            }
+            if (!saw_backslash) {
+                if (cp == '\\') {
+                    saw_backslash = true;
+                } else {
+                    newstr.append(Character.toChars(cp));
+                }
+                continue; /* switch */
+            }
+            if (cp == '\\') {
+                saw_backslash = false;
+                newstr.append('\\');
+                newstr.append('\\');
+                continue; /* switch */
+            }
+
+            switch (cp) {
+
+                case 'r':  newstr.append('\r');
+                    break; /* switch */
+
+                case 'n':  newstr.append('\n');
+                    break; /* switch */
+
+                case 'f':  newstr.append('\f');
+                    break; /* switch */
+
+                case 'b':  newstr.append("\\b");
+                    break; /* switch */
+
+                case 't':  newstr.append('\t');
+                    break; /* switch */
+
+                case 'a':  newstr.append('\007');
+                    break; /* switch */
+
+                case 'e':  newstr.append('\033');
+                    break; /* switch */
+
+                case 'c':   {
+                    if (++i == oldstr.length()) { die("trailing \\c"); }
+                    cp = oldstr.codePointAt(i);
+                    if (cp > 0x7f) { die("expected ASCII after \\c"); }
+                    newstr.append(Character.toChars(cp ^ 64));
+                    break; /* switch */
+                }
+
+                case '8':
+                case '9': die("illegal octal digit");
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7': --i;
+                case '0': {
+                    if (i+1 == oldstr.length()) {
+                        newstr.append(Character.toChars(0));
+                        break; /* switch */
+                    }
+                    i++;
+                    int digits = 0;
+                    int j;
+                    for (j = 0; j <= 2; j++) {
+                        if (i+j == oldstr.length()) {
+                            break; /* for */
+                        }
+                    /* safe because will unread surrogate */
+                        int ch = oldstr.charAt(i+j);
+                        if (ch < '0' || ch > '7') {
+                            break; /* for */
+                        }
+                        digits++;
+                    }
+                    if (digits == 0) {
+                        --i;
+                        newstr.append('\0');
+                        break; /* switch */
+                    }
+                    int value = 0;
+                    try {
+                        value = Integer.parseInt(
+                                oldstr.substring(i, i+digits), 8);
+                    } catch (NumberFormatException nfe) {
+                        die("invalid octal value for \\0 escape");
+                    }
+                    newstr.append(Character.toChars(value));
+                    i += digits-1;
+                    break; /* switch */
+                } /* end case '0' */
+
+                case 'x':  {
+                    if (i+2 > oldstr.length()) {
+                        die("string too short for \\x escape");
+                    }
+                    i++;
+                    boolean saw_brace = false;
+                    if (oldstr.charAt(i) == '{') {
+                        /* ^^^^^^ ok to ignore surrogates here */
+                        i++;
+                        saw_brace = true;
+                    }
+                    int j;
+                    for (j = 0; j < 8; j++) {
+
+                        if (!saw_brace && j == 2) {
+                            break;  /* for */
+                        }
+                        int ch = oldstr.charAt(i+j);
+                        if (ch > 127) {
+                            die("illegal non-ASCII hex digit in \\x escape");
+                        }
+
+                        if (saw_brace && ch == '}') { break; /* for */ }
+
+                        if (! ( (ch >= '0' && ch <= '9')
+                                ||
+                                (ch >= 'a' && ch <= 'f')
+                                ||
+                                (ch >= 'A' && ch <= 'F')
+                        )
+                                )
+                        {
+                            die(String.format(
+                                    "illegal hex digit #%d '%c' in \\x", ch, ch));
+                        }
+
+                    }
+                    if (j == 0) { die("empty braces in \\x{} escape"); }
+                    int value = 0;
+                    try {
+                        value = Integer.parseInt(oldstr.substring(i, i+j), 16);
+                    } catch (NumberFormatException nfe) {
+                        die("invalid hex value for \\x escape");
+                    }
+                    newstr.append(Character.toChars(value));
+                    if (saw_brace) { j++; }
+                    i += j-1;
+                    break; /* switch */
+                }
+
+                case 'u': {
+                    if (i+4 > oldstr.length()) {
+                        die("string too short for \\u escape");
+                    }
+                    i++;
+                    int j;
+                    for (j = 0; j < 4; j++) {
+                    /* this also handles the surrogate issue */
+                        if (oldstr.charAt(i+j) > 127) {
+                            die("illegal non-ASCII hex digit in \\u escape");
+                        }
+                    }
+                    int value = 0;
+                    try {
+                        value = Integer.parseInt( oldstr.substring(i, i+j), 16);
+                    } catch (NumberFormatException nfe) {
+                        die("invalid hex value for \\u escape");
+                    }
+                    newstr.append(Character.toChars(value));
+                    i += j-1;
+                    break; /* switch */
+                }
+
+                case 'U': {
+                    if (i+8 > oldstr.length()) {
+                        die("string too short for \\U escape");
+                    }
+                    i++;
+                    int j;
+                    for (j = 0; j < 8; j++) {
+                    /* this also handles the surrogate issue */
+                        if (oldstr.charAt(i+j) > 127) {
+                            die("illegal non-ASCII hex digit in \\U escape");
+                        }
+                    }
+                    int value = 0;
+                    try {
+                        value = Integer.parseInt(oldstr.substring(i, i+j), 16);
+                    } catch (NumberFormatException nfe) {
+                        die("invalid hex value for \\U escape");
+                    }
+                    newstr.append(Character.toChars(value));
+                    i += j-1;
+                    break; /* switch */
+                }
+
+                default:   newstr.append('\\');
+                    newstr.append(Character.toChars(cp));
+           /*
+            * say(String.format(
+            *       "DEFAULT unrecognized escape %c passed through",
+            *       cp));
+            */
+                    break; /* switch */
+
+            }
+            saw_backslash = false;
+        }
+
+    /* weird to leave one at the end */
+        if (saw_backslash) {
+            newstr.append('\\');
+        }
+
+        return newstr.toString();
+    }
+    private static final
+    void die(String foa) {
+        throw new IllegalArgumentException(foa);
+    }
 
 
 
     String[] languages = new String[] {
             "English",
-            "Assamese",
             "Bangla",
             "Gujarati",
-            "Gurumukhi",
             "Hindi",
             "Kannada",
-            "Malayalam",
-            "Manipuri",
-            "Marathi",
             "Tamil",
-            "Telugu"
+            "Telugu",
+            "Urdu"
     };
 
 
@@ -122,6 +344,13 @@ public class MainActivity extends Activity {
             public void onClick(View view) {
 
                 takePicture();
+                dialog = ProgressDialog.show(MainActivity.this, "", "Uploading file...", true);
+                new Thread(new Runnable() {
+                    public void run() {
+                        uploadFile(mFileTemp.getPath(), languageSpinner.getSelectedItem().toString());
+                    }
+                }).start();
+
             }
         });
 
@@ -305,7 +534,22 @@ public class MainActivity extends Activity {
                         Toast.makeText(MainActivity.this, globResponseString, Toast.LENGTH_SHORT).show();
                     }
                 });
-
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String str = globResponseString;
+                        /*AlertDialog.Builder alert = new AlertDialog.Builder(context);
+                        alert.setTitle("Failure");
+                        alert.setMessage(str);
+                        alert.show();*/
+                        AlertDialog dialog = new AlertDialog.Builder(context).setMessage("").show();
+                        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+                        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/Devlys_010.ttf");
+                        textView.setTypeface(face);
+                        str = unescape_perl_string(str);
+                        dialog.setMessage(str);
+                    }
+                });
             }
 
             @Override
@@ -315,9 +559,30 @@ public class MainActivity extends Activity {
                 dialog.dismiss();
                 Log.i("uploadFile", "Success!" + responseString);
                 globResponseString = responseString;
-                runOnUiThread(new Runnable() {
+                /*runOnUiThread(new Runnable() {
                     public void run() {
                         Toast.makeText(MainActivity.this, globResponseString, Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String str = globResponseString;
+                        Typeface face=Typeface.createFromAsset(getAssets(),"fonts/Ananda.ttf");
+                        AlertDialog dialog = new AlertDialog.Builder(context).setMessage("").show();
+                        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+                        textView.setTypeface(face);
+                        str = unescape_perl_string(str);
+                        dialog.setMessage(str);
+                        //dialog.setTitle("Success");
+                        //dialog.show();
+
+                        /*Typeface face=Typeface.createFromAsset(getAssets(),"fonts/Devlys_010.ttf");
+                        AlertDialog dialog = new AlertDialog.Builder(context).setMessage("").show();
+                        TextView textView = (TextView) dialog.findViewById(android.R.id.message);
+                        textView.setTypeface(face);
+                        dialog.setMessage(str);*/
                     }
                 });
             }
